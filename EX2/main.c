36f8b2a19fbee5a8480e1fab79058d160810aa71
Smaxoi <pengsam15@gmail.com>
2024-11-28 13:28:23 +0800

    main.c

---
+++ EX2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("    November 2024   \n");
+    printf("Mo Tu We Th Fr Sa Su\n");
+    printf("                1  2\n");
+    printf("10 11 12 13 14 15 16\n");
+    printf("17 18 19 20 21 22 23\n");
+    printf("24 25 26 27 28 29 30\n");
+    
+
+    return 0;
+}
