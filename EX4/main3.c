ec2c5a976ec9511558201c776efc0182eb1a735b
Smaxoi <pengsam15@gmail.com>
2024-12-12 15:26:33 +0800

    Create main3.c

---
+++ EX4/main3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int y = 1;
+    int x = 1;
+    
+    for(x = 1; x <= 9; ){
+            
+            printf( "%d*%d=%d\t", x, y,x * y);
+            y++;
+            if (y > 9) {
+                y = 1;
+                x++;
+                printf("\n");
+            }
+    }
+    return 0;
+}
