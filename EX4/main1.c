d83cc6b1923b923be050f628043ccf96954bb8fe
Smaxoi <pengsam15@gmail.com>
2024-12-12 15:07:45 +0800

    Update main1.c

--- EX4/main1.c
+++ EX4/main1.c
@@ -1,1 +1,26 @@
-e
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    int n = 7;
+    
+    for(x = 1; x <= 7; x++) {
+        
+        for(y = 1; y <= 7; y++){
+           
+           if(y <= n - x){
+               printf(" ");
+               
+           }
+           
+           else{
+               printf("%d ", x);
+               
+           }
+            
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
